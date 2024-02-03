08dbd8692d7c041258fde61ceca9b182a7cf0af8
ShivayAnkit <byankitsharma@gmail.com>
2024-02-03 11:19:31 +0530

    pushing prime code on github

---
+++ prime.js
@@ -0,0 +1,21 @@
+//program to find check prime number
+let count = 0;
+function checkPrime(num)
+{
+for(let i = 1; i < num; i++)
+{
+    if(num % i == 0)
+    {
+        count++;
+    }
+}
+if(count >= 2)
+{
+    console.log("Not Prime");
+}
+else
+{
+    console.log("prime");
+}
+}
+checkPrime(9);
